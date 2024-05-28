011ee7975afa20107a9b674e923b3a9250bb9c71
RAMISH0101 <ramishrfq@gmail.com>
2024-05-28 19:30:32 +0500

    file edited

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='pop'>
+        I LOVE YOU NIDAAAAAAAAAA &#9829; &#9829; &#9829; &#9829; &#9829;
+      </p>
+      <div className='im'>
+      <img  src="https://media.tenor.com/ZhNxfL0GmoMAAAAj/mocha-bear-hearts.gif" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
